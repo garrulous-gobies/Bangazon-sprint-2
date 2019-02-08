89fa2b7f46363208e8e48c71e4ad9df3f598e112
amazoradi <amazoradi@gmail.com>
2019-02-08 09:14:11 -0600

    number of items in category displaying next to name

--- website/views/products_views.py
+++ website/views/products_views.py
@@ -14,18 +14,25 @@ def list_products(request):
     return render(request, template_name, {'products': all_products})
 
 def categories(request):
-    sql = """ SELECT * 
-              FROM "website_product"
+    """ Summary: This view selects all product categories and teh first three products. IT will display the categories, with the number of products in it, and the first three products.
+
+    Modal(s): ProductType, Product
+
+    Template: categories.html
+
+    Author(s): Austin Zoradi
     """
 
-    products_in_cat = """SELECT count(website_product.id)
-                         FROM "website_product"
-                         WHERE website_product.productType_id = website_producttype.id
+    sql = """ SELECT * 
+              FROM "website_product"
+              
     """
-    sql2 = """SELECT * FROM "website_producttype"
+
+    sql2 = """SELECT *
+              FROM "website_producttype"
     """
-    product_in_category = Product.objects.raw(products_in_cat)
+
     all_products = Product.objects.raw(sql)
     all_productTypes = ProductType.objects.raw(sql2)
-    context = {"all_products": all_products, "all_productTypes": all_productTypes, "product_in_category":product_in_category}
+    context = {"all_products": all_products, "all_productTypes": all_productTypes}
     return render(request, 'categories.html', context)
