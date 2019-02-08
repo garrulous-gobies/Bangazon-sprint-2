499ae7dbd12e4e5eda9e0272c39c842c01520cec
Nolan-Little <little.nolan@gmail.com>
2019-02-08 11:18:52 -0600

    product category selection

--- website/views/sell_views.py
+++ website/views/sell_views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import RequestContext
 from django.db import connection
+from django.urls import reverse
 
 from django.contrib.auth.models import User
 from website.models import Product, Customer, ProductType
@@ -18,40 +19,44 @@ def sell_product(request):
 
     elif request.method == 'POST':
         form_data = request.POST
-        category = ProductType.objects.get(pk=1)
 
         p_form_data = {
             'title': form_data['title'],
             'description': form_data['description'],
             'price': form_data['price'],
             'quantity': form_data['quantity'],
+            'category': form_data['category']
         }
+
         product_form = ProductForm(p_form_data)
 
         if product_form.is_valid():
             print('++++++++++++++IM IN THE IF, SO IM VALID++++++++++++++++')
             customer_id = request.user.id
-            productType_id = 1
 
-            sql = "INSERT INTO website_product VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+            sql = "INSERT INTO website_product VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
             product_params = [
                 None,
                 p_form_data['title'],
                 p_form_data['description'],
                 p_form_data['price'],
                 p_form_data['quantity'],
-                0,
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEbVj-dY-HFt1DhTc2JXe0LtqQ-HSDyMy1qvDNWJoFMtB7gfR9',
+                0,    #default boolean not deleted
                 customer_id,
-                productType_id
+                p_form_data['category']
             ]
 
             with connection.cursor() as cursor:
                 cursor.execute(sql, product_params)
+                product_id = cursor.lastrowid
         else:
 
             print("=============INVALID FORM DATA=========",
                   product_form.errors.as_json())
             template_name = 'product/create.html'
             return render(request, template_name, {'product_form': product_form})
-        template_name = 'product/success.html'
-        return render(request, template_name, {})
+
+        product = Product.objects.raw('SELECT * FROM website_product WHERE id = %s', [product_id])
+        template_name = 'product_details.html'
+        return HttpResponseRedirect(reverse('website:product_details', args=(product_id,)))
