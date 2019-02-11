74cf256db3a1f4c1764883d48c3ec8efdddf1558
Nolan-Little <little.nolan@gmail.com>
2019-02-11 13:11:53 -0600

    order details displays product line items with price and order total:

--- website/views/order_history.py
+++ website/views/order_history.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from website.models import Order
 from django.shortcuts import render
+from decimal import Decimal
 
 
 def order_history(request, pk):
@@ -29,5 +30,35 @@ def order_history_details(request, pk, order_id):
         WHERE customerOrder_id = %s AND id = %s
     '''
     order = Order.objects.raw(get_single_order_sql, [pk, order_id])[0]
-    context = {'order': order}
+
+    order_total = get_order_total(order)
+    context = {'order': order, 'order_total': order_total}
     return render(request, 'order_history_detail.html', context)
+
+
+
+# ===========================helper functions========================
+def get_order_total(order):
+    """Calculates total of all the product prices and tax in the gven order
+
+    Author(s): Nolan Little
+
+    Arguments:
+        order {Object} -- Single Order instance from the database
+
+    Returns:
+        dictionary -- sub_total = combined price of all objects, tax = calculated tax, total = final total(sub + tax)
+    """
+
+    tax_rate = Decimal(.0945)
+    sub_total = 0
+    order_total = dict()
+
+    for product_order in order.productorder_set.all():
+        sub_total += product_order.product.price
+
+    order_total['sub_total'] = sub_total
+    order_total['tax'] = round(sub_total * tax_rate, 2)
+    order_total['total'] = round(sub_total + (sub_total * tax_rate), 2)
+
+    return order_total
